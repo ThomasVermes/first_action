37b2524094500fbaf1df8351043aea1d7f4210b1
ThomasVermes <75424115+ThomasVermes@users.noreply.github.com>
2024-03-28 15:48:26 +0100

    Update script.py

--- .github/workflows/script.py
+++ .github/workflows/script.py
@@ -12,8 +12,12 @@ import os
 new_version = os.environ.get('RELEASE_TAG')
 new_release_date = datetime.datetime.now().strftime('%Y-%m-%d')
 
+# Ottieni il percorso assoluto del file CITATION.cff
+script_dir = os.path.dirname(os.path.abspath(__file__))
+cff_file_path = os.path.join(script_dir, "../../CITATION.cff")
+
 # Leggi il contenuto del file CITATION.cff
-with open('../../CITATION.cff', 'r') as file:
+with open(cff_file_path, 'r') as file:
     cff_data = yaml.safe_load(file)
 
 # Aggiorna i valori dei campi version e date-released
@@ -21,5 +25,5 @@ cff_data['version'] = new_version
 cff_data['date-released'] = new_release_date
 
 # Sovrascrivi il file con i dati aggiornati
-with open('../../CITATION.cff', 'w') as file:
+with open(cff_file_path, 'w') as file:
     yaml.dump(cff_data, file)
