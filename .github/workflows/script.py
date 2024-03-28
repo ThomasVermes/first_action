cfe99260325be77ba9226cc494e73eb0ecb342a8
ThomasVermes <75424115+ThomasVermes@users.noreply.github.com>
2024-03-28 15:41:11 +0100

    Update script.py

--- .github/workflows/script.py
+++ .github/workflows/script.py
@@ -1,4 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 26 11:44:39 2024
+
+@author: vermest
+"""
+
+#Clear old variables
+globals().clear()
+import yaml
+import datetime
 import os
-release_tag = os.environ.get('RELEASE_TAG')
-print("Hello python world")
-print("tag from env var: ", release_tag)
+
+# Definisci i nuovi valori per versione e data di rilascio
+new_version = os.environ.get('RELEASE_TAG')
+new_release_date = datetime.datetime.now().strftime('%Y-%m-%d')
+
+# Leggi il contenuto del file CITATION.cff
+with open('../CITATION.cff', 'r') as file:
+    cff_data = yaml.safe_load(file)
+
+# Aggiorna i valori dei campi version e date-released
+cff_data['version'] = new_version
+cff_data['date-released'] = new_release_date
+
+# Sovrascrivi il file con i dati aggiornati
+with open('../CITATION.cff', 'w') as file:
+    yaml.dump(cff_data, file)
