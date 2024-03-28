92ef6b68e8b8cd674e6595cdc0003b2d911a2dc5
ThomasVermes <75424115+ThomasVermes@users.noreply.github.com>
2024-03-28 15:44:53 +0100

    Update script.py

--- .github/workflows/script.py
+++ .github/workflows/script.py
@@ -13,7 +13,7 @@ new_version = os.environ.get('RELEASE_TAG')
 new_release_date = datetime.datetime.now().strftime('%Y-%m-%d')
 
 # Leggi il contenuto del file CITATION.cff
-with open('../CITATION.cff', 'r') as file:
+with open('../../CITATION.cff', 'r') as file:
     cff_data = yaml.safe_load(file)
 
 # Aggiorna i valori dei campi version e date-released
@@ -21,5 +21,5 @@ cff_data['version'] = new_version
 cff_data['date-released'] = new_release_date
 
 # Sovrascrivi il file con i dati aggiornati
-with open('../CITATION.cff', 'w') as file:
+with open('../../CITATION.cff', 'w') as file:
     yaml.dump(cff_data, file)
